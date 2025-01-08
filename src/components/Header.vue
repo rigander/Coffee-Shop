e07b0bae74a8a3d5af5144d2eac3b168d184f375
Andrey Komov <andrewkomov@gmail.com>
2025-01-08 23:35:16 +0200

    added required fonts, sattled @ alias, began styling HTML

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,7 +5,15 @@
 
 <template>
   <header>
-      <div></div>
+      <img id="top-logo" src="/img/top-logo.png" alt="logo">
+      <nav>
+          <ul class="top-nav">
+              <li><a href="">WELCOME</a></li>
+              <li><a href="">MENU</a></li>
+              <li><a href="">EVENTS</a></li>
+              <li><a href="">CONTACT</a></li>
+          </ul>
+      </nav>
   </header>
 </template>
 
