a7e9efd484d04abab16bb5c7729baa0ed38ff95a
Andrey Komov <andrewkomov@gmail.com>
2025-01-11 02:18:11 +0200

    main content layout

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -34,6 +34,94 @@
               </div>
           </div>
       </section>
+      <section class="hot-cold-coffee-deserts">
+          <article class="hot-coffee">
+              <h2>Hot Coffee</h2>
+              <p>
+                  Lorem ipsum dolor sit amet, consecte
+                  sectetur adipisicing elit, tation omne
+                  ullamco laboris nisi ut aliqolore.
+              </p>
+          </article>
+          <article class="cold-coffee">
+              <h2>Cold Coffee</h2>
+              <p>
+                  Lorem ipsum dolor sit amet, consecte
+                  sectetur adipisicing elit, tation omne
+                  ullamco laboris nisi ut aliqolore.
+              </p>
+          </article>
+          <article class="dessert">
+              <h2>Dessert</h2>
+              <p>
+                  Lorem ipsum dolor sit amet, consecte
+                  sectetur adipisicing elit, tation omne
+                  ullamco laboris nisi ut aliqolore.
+              </p>
+          </article>
+      </section>
+      <section class="traditional-coffee">
+          <h1>Traditional Coffee</h1>
+          <p>
+              Lorem ipsum dolor sit amet, consecte
+              sectetur adipisicing elit, tation omne
+              ullamco laboris nisi ut aliqolore.
+          </p>
+          <div class="sawtooth">
+              <img src="/img/coffee-beans-icon.png" alt="icon">
+              <h2>100% ARABICA</h2>
+          </div>
+      </section>
+      <section class="chocolate">
+          <article>
+              <h3 class="tart-berry_date">Food / 16th June, 2024</h3>
+              <h3 class="tart-berry_title">Lorem ipsum dolor sit</h3>
+              <p>Lorem ipsum dolor sit amet,
+                 consectetur adipisicing elit, sed
+              </p>
+              <button>READ MORE</button>
+          </article>
+      </section>
+      <section class="book-here">
+          <div class="book-here_title">
+              BOOK HERE
+          </div>
+      </section>
+      <form action="">
+          <div class="num-of-per">
+              <label class="num-of-per_label" for="num-of-per">
+                How many people?
+              </label>
+              <select id="num-of-per">
+                  <option value="option1">1 person</option>
+                  <option value="option2" selected>2 people</option>
+                  <option value="option3">3 people</option>
+              </select>
+          </div>
+          <div class="date">
+              <label class="date_label" for="date">
+                When is your date?
+              </label>
+              <input type="date"/>
+          </div>
+          <div class="time">
+              <label for="time">Time</label>
+              <input type="time">
+          </div>
+          <div class="full-name">
+              <label for="full-name">Name</label>
+              <input name="full-name" type="text">
+          </div>
+          <div class="email">
+              <label for="email">Email</label>
+              <input name="email" type="email">
+          </div>
+          <div class="phone">
+              <label for="phone">Phone</label>
+              <input name="phone" type="number">
+          </div>
+          <button class="submit" type="submit">SUBMIT</button>
+      </form>
   </div>
 </template>
 
